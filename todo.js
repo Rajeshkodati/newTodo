cb912142ed0a9268d978c90e5d45a88166e0b243
rajesh <rajesh.kodati@gmail.com>
2019-02-01 22:11:19 +0530

    updaet for check option

--- todo.js
+++ todo.js
@@ -1,23 +1,23 @@
 const inputValue = document.querySelector('.todo-value');
 var Enter_key =13;
 inputValue.addEventListener('keypress',createListItem);
-
+var itemCount = 0;
 
 function createListItem(event){
      
     if(event.charCode === Enter_key){
          if(!inputValue.value.trim()){
-            
+            alert('Enter text');
         }
         else{
             addItems();
-            document.querySelector('#footer').style.display = 'block'
+            
+            document.querySelector('#footer').style.display = 'block';
             inputValue.value ='';
         }
           }
 }
 function addItems(){
-
     const listItem = document.createElement('li');
     const addDiv = document.createElement('div');
     const destroy = document.createElement('button');
@@ -29,6 +29,7 @@ function addItems(){
     destroy.classList.add('destroy');
     destroy.textContent = 'del';
     label.textContent = inputValue.value;
+    console.log(document.querySelector('.count').innerHTML = ++itemCount);
     addDiv.appendChild(editItem);
     addDiv.appendChild(label);
     addDiv.appendChild(destroy);
@@ -53,8 +54,28 @@ function addItems(){
 //  document.querySelector('.complete').addEventListener('click',function(){
 //     addDiv.style.display ='none';
 //  });
+editItem.addEventListener('click',editItemvalue);
 }
 function deleteItem(){
+    document.querySelector('.count').textContent = --itemCount;
     this.parentNode.remove();
 }
 
+function editItemvalue(){
+    document.querySelector('.clear').style.display = 'block';
+    document.querySelector('.count').textContent = --itemCount;
+    document.querySelector('.view').classList.toggle('selected')
+    if(itemCount === 0){
+        document.querySelector('.count').textContent = '';
+          removeEvent();
+    }
+    document.querySelector('.clear').addEventListener('click',clearItemList);
+}
+function clearItemList(){
+   console.log(document.querySelector('.selected').remove());
+}
+function removeEvent(){
+    if(itemCount === 0){
+        document.querySelector('.toggle').removeEventListener('click',editItemvalue);
+    }
+}
