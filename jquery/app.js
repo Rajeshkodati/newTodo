c1d8ce7c6f6af254ead5e9e6227660f1277ff670
rajesh <rajesh.kodati@gmail.com>
2019-02-05 11:09:31 +0530

    error problem

--- jquery/app.js
+++ jquery/app.js
@@ -3,44 +3,41 @@ $(document).ready(function(){
     const keyValue = 13;
     const inputValue = $('.item-input');
  $('.item-input').keypress(function(event){
-     if(event.charCode === keyValue){
-        
-     if(!$('.item-input').val().trim()){
-        alert('enter value');
-   } 
-   else{
-      const addItems = '<li class = add-item>'+
-     '<div class = view>'+
-        '<input type =checkbox class=item-check>'+
-        '<span class = item-text>'+inputValue.val()
-        +'</span>'+
-        '<button class = delete>Del</button>'
-     '</div>'     
-    '</li>'
-    $('.list-items').append(addItems);
-    $('.count').html(++itemNumber);
-     inputValue.val(''); 
-    $('footer').css({display:'block'});
-    
+    if(event.charCode === keyValue){
+        if(!$('.item-input').val().trim()){
+            alert('enter value');
+            return;
+        }else{
+            const addItems = '<li class = add-item>'+
+            '<div class = view>'+
+                '<input type =checkbox id=item-check>'+
+                '<span class = item-text>'+inputValue.val()
+                +'</span>'+
+                '<button class = delete>Del</button>'
+            '</div>'     
+        '</li>'
+            $('.list-items').append(addItems);
+            $('.count').html(++itemNumber);
+            inputValue.val(''); 
+            $('footer').css({display:'block'});
+        }
     }
-  }
-  $('.delete').click(function(){
-      $(this).parent().remove();
-  });
-  $('.view').dblclick(function(){
-      $('.item-check,.item-text,.delete').hide();
-      $('.view').append('<input type = text class = edit>');
-      $('.edit').blur(function(){
-        $('.item-text').html($(this).val());
-        $(this).hide();
-        $('.item-check,.item-text,.delete').show();
-      });
-  });
-   $('.item-check').change(function(){
-        console.log($(this.checked),'hello')
-       $('.clear').css({display:'block'});
-    
-   });
- });
+    $('.delete').click(function(){
+        $(this).parent().remove();
+    });
+    $('.view').dblclick(function(){
+        $('#item-check,.item-text,.delete').hide();
+        $('.view').append('<input type = text class = edit>');
+        $('.edit').blur(function(){
+            $('.item-text').html($(this).val());
+            $(this).hide();
+            $('.item-check,.item-text,.delete').show();
+        });
+    });
 
+    });
+    $('#item-check').on('click',function(){
+        console.log($(this.checked),'hello');
+        $('.clear').css({display:'block'});
+    });
 });
